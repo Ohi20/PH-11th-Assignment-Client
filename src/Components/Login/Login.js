5f2ae3b2b8709c40998601a6cc3bf4610fb477d4
Ohi97 <ashrafohi20@gmail.com>
2022-05-07 22:28:19 +0600

    created fakedata

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,12 +3,30 @@ import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleAdduser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.name.value;
+        const user = {name, email};
+        // send data to server
+        fetch('http://localhost:5000/user',{
+            mathod: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('success', data)
+        })
+    }
     return (
         <div>
             <div className='container w-50 mx-auto'>
             
             <h2 className='text-primary text-center mt-2'>Please Login</h2>
-            <Form>
+            <Form onSubmit={handleAdduser}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Control  type="email" placeholder="Enter email" required />
                 </Form.Group>
