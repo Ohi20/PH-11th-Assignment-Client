f2dd7dc542e547b56787d23b0bbbd0a45d1a4581
Ohi20 <ashrafohi20@gmail.com>
2022-05-09 22:27:04 +0600

    button added

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Card, CardGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import useServices from '../Hooks/useServices';
 
 const Inventory = () => {
     const [services, setServices] = useServices();
+
+    const navigate = useNavigate();
     return (
         
       <div className=' m-5'>
@@ -11,16 +13,20 @@ const Inventory = () => {
         
         <ul>
           {
-            services.map(service => <div className='container border my-5 bg-dark p-4' key={service._id}>
+            services.slice(0,6).map(service => <div className='container border my-5 bg-dark p-4' key={service._id}>
               <h2 className='text-white'>{service.name}</h2>
               <img className='w-50 rounded-5' src={service.image} srcSet=''/>
               <h2 className='text-white mt-4'>Price:{service.price}</h2>
               <p className='text-white'>Description: {service.shortDescription}</p>
               <h2 className='text-white'>Quantity:{service.quantity}</h2>
               <h3 className='text-white'>Sold yet?: {service.sold}</h3>
+              <button className='btn-info text-white my-3'>Update</button>
             </div>)
           }
         </ul>
+        <div>
+          <button onClick={()=>navigate("/services")} className='btn-info text-white'>Manage Inventories</button>
+        </div>
       </div>
         
 
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,47 +1,56 @@
 import React from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import useServices from '../Hooks/useServices';
 
 const Services = () => {
     const [services, setServices] = useServices();
     
-    return (
+    const handleInventoryDelete = id => {
+        const proceed = window.confirm('Are you sure?');
+        if(proceed){
+          const url = `http://localhost:5000/service/${id}`;
+          fetch(url, {
+            method:'DELETE'
+          })
+          .then(res => res.json())
+          .then(data=>{
+            if(data.deletedCount > 0){
+              const remaining = services.filter(service => service._id !== id);
+              setServices(remaining);
+            }
+          })
+        } 
+    }
        
-        <div className='col-12'>
-            
+      
+      return (
+          
+        <div className=' m-5'>
+          <h2 className='text-white'>Available Inventory</h2>
+          <h2 className='text-white'>Quantity: {services.length} </h2>
+          <div className='my-3'>
+        <input type="text" placeholder="restock the items" required="" id="username" />
+        <input className='bg-info text-white' type="submit" value="Restock" />
+      </div>
+          <ul>
             {
-                services.map(service => <div className='container-fluid col-12' key={service._id}>
-                    
-                    <div className='col-12'>
-                    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={service.image} />
-  <Card.Body>
-    <Card.Title><h5 className='text-info'>{service.name}</h5></Card.Title>
-    <Card.Text>
-    <p className='text-info'>Sold yet?{service.sold}</p>
-    </Card.Text>
-  </Card.Body>
-  <ListGroup className="list-group-flush">
-    <ListGroupItem><p className='text-info'>Price:${service.price}</p></ListGroupItem>
-    <ListGroupItem><p className='text-info'>Quantity:{service.quantity}</p></ListGroupItem>
-    </ListGroup>
-  <Card.Body>
-    <Card.Link href="#">Card Link</Card.Link>
-    <Card.Link href="#">Another Link</Card.Link>
-  </Card.Body>
-</Card>
-                    </div>
-                    
-                    
-                    
-                    </div>
-           
+              services.map(service => <div className='container border my-5 bg-dark p-4' key={service._id}>
+                <h2 className='text-white'>{service.name}</h2>
+                <img className='w-50 rounded-5' src={service.image} srcSet=''/>
+                <h2 className='text-white mt-4'>Price:{service.price}</h2>
+                <p className='text-white'>Description: {service.shortDescription}</p>
+                <h2 className='text-white'>Quantity:{service.quantity}</h2>
+                <h3 className='text-white'>Sold yet?: {service.sold}</h3>
+                <button onClick={()=> handleInventoryDelete(services._id)} className='btn-info text-white my-3'>Delivered</button>
+              </div>)
+            }
+          </ul>
+        </div>
         )
     }
 
 
-    </div>
-    );
-};
+    
+    
+
 
 export default Services;
